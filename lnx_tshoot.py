16efe5a21650b0a3eeeaa3e5e85f991b29c4d2a1
Hammad <jilanih1@gmail.com>
2017-07-05 17:01:40 -0500

    new file:   lnx_tshoot.py Pseudocode

---
+++ lnx_tshoot.py
@@ -0,0 +1,30 @@
+#Linux Troubleshooting script - run basic Linux commands via python script
+
+### Pseudocode ###
+
+# Modules
+
+# Global variables.
+# paramiko
+
+# Class for colors.
+
+# Define a function for command menu:
+	# 1. Check system uptime.
+	# 2. Check free memory.
+	# 3. Check if a process is running.
+	# 4. Check for a string in /var/log/messages.
+	# 5. Exit.
+
+# Define ssh function.
+
+# Add options for hostname and username.
+# raw_inputs for if no option is selected.
+# getpass to enter password.
+
+# if/else statement to ping the host
+	# if pingable:
+		# while loop to slected an option in menu and run the command.
+			# if/else statement for choices.
+	# else:
+		# exit.
