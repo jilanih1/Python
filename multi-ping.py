cb18cdef373374f7401bfebc00258b1a338ded3c
Hammad <jilanih1@gmail.com>
2017-07-13 13:04:58 -0500

    Rewrote script: -Defined main(): -Removed argparse. -Now using sys.argv for argument. -If no argument is entered, user is now able to input one. -Script is now compatible in Python2&3. -Misc. clean-up.

--- multi-ping.py
+++ multi-ping.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import print_function #Python3 Compatibality
-import os, sys, subprocess, argparse #Modules
+from __future__ import print_function #Python Version Compatibility
+import os, sys, subprocess #Modules
+
+if sys.version_info[:2] <= (2, 7): #Python Version Compatibility
+	input = raw_input
 
 #Colors, Green=Pass, Red=Fail
 class colors():
@@ -9,48 +12,42 @@ class colors():
 	Green = '\033[92m'
 	Reset = '\033[0m'
 
-#Defined to make code easier to read
+#Information messages:
 class info():
-	nofile = 'Please select a file'
+	nofile = 'Please select a file: '
 	vafile = ' is a valid file, proceeding...'
 	fafile = 'File not found. Please check filename and path, exiting...'
-	success = ' is pingable'
-	fail = ' is not pingable'
+	pingsu = ' is pingable'
+	pingfl = ' is not pingable'
 
-#Allows -f + <file> while running the script to choose a file without prompt.
-parser = argparse.ArgumentParser(usage='multi-ping -f <file>')
-required = parser.add_argument_group('required arguments')
-required.add_argument('-f', '--file', help='specify the file with full path')
-args = parser.parse_args()
-
-#Checks if an argument is provided for -f
-if not args.file:
-	print(colors.Red + info.nofile + colors.Reset + '\n')
-	sys.exit(1)
+if len(sys.argv) > 1:
+	hfile = sys.argv[1]
+else:
+	hfile = input(colors.Red + info.nofile + colors.Reset)
 
 #Checks if the argument provided is valid
-if os.path.isfile(args.file):
-	print(colors.Green + args.file + info.vafile + colors.Reset + '\n')
+if os.path.isfile(hfile):
+	print(colors.Green + hfile + info.vafile + colors.Reset)
 else:
-	print(colors.Red + info.fafile + colors.Reset + '\n')
+	print(colors.Red + info.fafile + colors.Reset)
 	sys.exit(1)
 
-#Opens the file and adds the entries to a list
-with open(args.file, 'r') as hosts:
-	hostlist = [line.strip() for line in hosts]
-hosts.close()
+def main():
+	#Opens the file and adds the entries to a list
+	with open(hfile, 'r') as hosts:
+		hostlist = [line.strip() for line in hosts]
+	hosts.close()
+	#for loop to ping all the entries in the list
+	for ip in hostlist:
+		output = subprocess.Popen(['ping', '-c', '1', ip],stdout=subprocess.PIPE)
+		stdout, stderr = output.communicate()
+		if output.returncode == 0:
+			print(colors.Green + ip + info.pingsu + colors.Reset)
+		else:
+			print(colors.Red + ip + info.pingfl + colors.Reset)
 
-#for loop to ping all the entries in the list
-for ip in hostlist:
-	output = subprocess.Popen(['ping', '-c', '1', ip],stdout=subprocess.PIPE)
-	stdout, stderr = output.communicate()
-
-	if output.returncode == 0:
-		print(colors.Green + ip + info.success + colors.Reset)
-	else:
-		print(colors.Red + ip + info.fail + colors.Reset)
-
-print('\n')
+if __name__ == '__main__':
+	main()
 sys.exit(0)
 
 ### NOTE: Hosts should be listed in the file as following (Without #) ###
